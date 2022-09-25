57e74dc3d452868ef14c746a336dc6c3bc8d0f94
Kirill <macgenr0@gmail.com>
2022-09-25 15:59:03 +0300

    add favicon & meta-tags

--- src/minesweeperMain.js
+++ src/minesweeperMain.js
@@ -9,85 +9,88 @@ import MusicComponents from "./Core/Modules/MusicComponents.js";
 import endGame from "./Core/Modules/endGame.js";
 
 /*============================================================================================================*/
-//! globals variables
-const globalGameData = new GlobalGameData();
+window.addEventListener('DOMContentLoaded', () => {
 
-// create visual board for user
-createBoard(15, 15, 35);
+	//! globals variables
+	const globalGameData = new GlobalGameData();
 
-//! start game
-activatorGameStatesMode(createBoard);
-/*============================================================================================================*/
+	// create visual board for user
+	createBoard(15, 15, 35);
 
-//? buttons animation
-globalGameData.buttonsParentDiv.addEventListener('click', event => {
-	event.preventDefault();
+	//! start game
+	activatorGameStatesMode(createBoard);
+	/*============================================================================================================*/
 
-	globalGameData.field.childNodes.forEach(item => {
-		item.classList.remove('bomb-cell');
+	//? buttons animation
+	globalGameData.buttonsParentDiv.addEventListener('click', event => {
+		event.preventDefault();
+
+		globalGameData.field.childNodes.forEach(item => {
+			item.classList.remove('bomb-cell');
+		});
+		globalGameData.field.classList.remove('blocked');
 	});
-	globalGameData.field.classList.remove('blocked');
-});
 
 
-//? clicks animation
-globalGameData.field.addEventListener('click', event => {
-	event.preventDefault();
-	const selector = event.target;
-	MusicComponents.musicSounds('../music/clicks.wav');
-	if (selector.tagName !== 'DIV') return;
+	//? clicks animation
+	globalGameData.field.addEventListener('click', event => {
+		event.preventDefault();
+		const selector = event.target;
+		MusicComponents.musicSounds('../music/clicks.wav');
+		if (selector.tagName !== 'DIV') return;
 
-	endGame(selector);
+		endGame(selector);
 
-	//create array with cells
-	globalGameData.getArrayChildrenCells();
-	globalGameData.getTargetIndex();
-	globalGameData.getBoardWidth();
-	globalGameData.getNumberBoardColumn();
-	globalGameData.getNumberBoardRow();
-	globalGameData.getBombsCount();
+		//create array with cells
+		globalGameData.getArrayChildrenCells();
+		globalGameData.getTargetIndex();
+		globalGameData.getBoardWidth();
+		globalGameData.getNumberBoardColumn();
+		globalGameData.getNumberBoardRow();
+		globalGameData.getBombsCount();
 
-	let bombsArray = [];
-	let bombsFirstClickAnimationArray = [];
+		let bombsArray = [];
+		let bombsFirstClickAnimationArray = [];
 
-	if (!globalGameData.field.classList.contains('blocked')) {
-		bombsFirstClickAnimationArray = bombsFirstClickAnimation(globalGameData.row,
-			globalGameData.column, globalGameData.WIDTH, globalGameData.BOMBS_COUNT,
-			globalGameData.cells);
+		if (!globalGameData.field.classList.contains('blocked')) {
+			bombsFirstClickAnimationArray = bombsFirstClickAnimation(globalGameData.row,
+				globalGameData.column, globalGameData.WIDTH, globalGameData.BOMBS_COUNT,
+				globalGameData.cells);
+
+			try {
+				bombsFirstClickAnimationArray.forEach(item =>
+					globalGameData.cells[item].classList.add('bomb-cell'));
+			} catch (error) {
+				alert('Something going wrong, game will restarting');
+				setTimeout(() => { window.location.reload() }, 1000);
+			}
+		}
+
+		globalGameData.field.childNodes.forEach((item, index) => {
+			if (typeof item.classList[2] === "undefined") {
+				return;
+			} else bombsArray.push(index);
+		});
 
 		try {
-			bombsFirstClickAnimationArray.forEach(item =>
-				globalGameData.cells[item].classList.add('bomb-cell'));
-		} catch (error) {
-			alert('Something going wrong, game will restarting');
-			setTimeout(() => { window.location.reload() }, 1000);
+			openFieldCells(globalGameData.row, globalGameData.column, selector,
+				globalGameData.WIDTH, globalGameData.cells, bombsArray);
+			isBomb(globalGameData.row, globalGameData.column, globalGameData.WIDTH,
+				bombsArray);
+			isValidForOpenCells(globalGameData.row, globalGameData.column, globalGameData.WIDTH,
+				bombsArray);
+			getCellsCount(globalGameData.row, globalGameData.column, globalGameData.WIDTH,
+				bombsArray);
+		} catch (err) {
+			alert("Please, restart");
+			window.location.reload();
 		}
-	}
-
-	globalGameData.field.childNodes.forEach((item, index) => {
-		if (typeof item.classList[2] === "undefined") {
-			return;
-		} else bombsArray.push(index);
 	});
 
-	try {
-		openFieldCells(globalGameData.row, globalGameData.column, selector,
-			globalGameData.WIDTH, globalGameData.cells, bombsArray);
-		isBomb(globalGameData.row, globalGameData.column, globalGameData.WIDTH,
-			bombsArray);
-		isValidForOpenCells(globalGameData.row, globalGameData.column, globalGameData.WIDTH,
-			bombsArray);
-		getCellsCount(globalGameData.row, globalGameData.column, globalGameData.WIDTH,
-			bombsArray);
-	} catch (err) {
-		alert("Please, restart");
-		window.location.reload();
-	}
-});
+	// right click animation 
+	globalGameData.field.addEventListener('contextmenu', (event) => {
+		event.preventDefault();
+		flagCounter(event);
+	});
 
-// right click animation 
-globalGameData.field.addEventListener('contextmenu', (event) => {
-	event.preventDefault();
-	flagCounter(event);
 });
-
