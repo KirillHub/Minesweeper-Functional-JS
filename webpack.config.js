eae06b3ee359d55f8e29300df2b5b836e86658dc
Kirill <macgenr0@gmail.com>
2022-10-07 12:57:44 +0300

    add webpack babel core.js

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let path = require('path');
+
+const sourseWay = './src/minesweeperMain.js';
+
+module.exports = {
+	mode: 'development',
+	entry: sourseWay,
+	output: {
+		filename: 'bundle.js',
+		path: __dirname + '/dist/js'
+	},
+	watch: true,
+
+	devtool: "source-map",
+
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [['@babel/preset-env', {
+							debug: true,
+							corejs: 3,
+							useBuiltIns: "usage"
+						}]]
+					}
+				}
+			}
+		]
+	}
+};
